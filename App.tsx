1a277a30f1ea038b94eecf2d846cb466afe14d87
nkr14 <naveenakrish14@gmail.com>
2025-11-18 19:43:20 -0500

    # Create the necessary folders mkdir -p src/pages src/components src/components/ui src/assets public

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blogs from "./pages/Blogs";
+import FundriveBlog from "./pages/FundriveBlog";
+import BOD from "./pages/BOD";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Blogs />} />
+          <Route path="/blog/fundrive" element={<FundriveBlog />} />
+          <Route path="/board-of-directors" element={<BOD />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
