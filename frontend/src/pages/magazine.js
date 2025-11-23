c2140419309177601f4324575c576afeb2edc5fa
nkr14 <naveenakrish14@gmail.com>
2025-11-23 14:50:11 -0500

    Update magazine.js
    Replaced flipbook with magazine PDF

--- frontend/src/pages/magazine.js
+++ frontend/src/pages/magazine.js
@@ -7,10 +7,39 @@ const Magazine = () => {
     return(
         <div className="magazine-page">
             <Nav/>
-                <iframe title="magazine" allowfullscreen allow="clipboard-write" scrolling="no" className="fp-iframe" style={{ border: "1px solid lightgray", width: "100%", height: "600px" }} src="https://heyzine.com/flip-book/19cebe0029.html"></iframe>         
+            <div className="pdf-container" style={{ 
+                width: "100%", 
+                maxWidth: "1200px", 
+                margin: "0 auto",
+                padding: "20px"
+            }}>
+                <object
+                    data="/path-to-your-magazine.pdf#view=FitH"
+                    type="application/pdf"
+                    width="100%"
+                    height="800px"
+                    style={{ 
+                        border: "1px solid lightgray",
+                        minHeight: "800px"
+                    }}
+                >
+                    <iframe
+                        src="/magazine.pdf#view=FitH"
+                        width="100%"
+                        height="800px"
+                        style={{ border: "1px solid lightgray" }}
+                        title="Magazine PDF"
+                    >
+                        <p>
+                            Your browser doesn't support PDF viewing. 
+                            <a href="/path-to-your-magazine.pdf">Download the PDF</a> instead.
+                        </p>
+                    </iframe>
+                </object>
+            </div>
             <Footer/>
         </div>
     )
 }
 
-export default Magazine; 
+export default Magazine;
