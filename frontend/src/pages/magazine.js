17888ecf8bdce6f42ddb43af9d955667b79af1b9
nkr14 <naveenakrish14@gmail.com>
2025-11-25 09:12:11 -0500

    Update magazine.js

--- frontend/src/pages/magazine.js
+++ frontend/src/pages/magazine.js
@@ -1,39 +1,32 @@
 // UPDATED TO PDF INSTEAD OF FLIPBOOK
 
-import Nav from '../components/Navbar.js'
+import Navbar from '../components/Navbar.js';
 import Footer from "../components/Footer.js";
 import MagazinePDF from '../images/magazine.pdf';
+import './Magazine.css';
 
 const Magazine = () => {
     return(
         <div className="magazine-page">
-            <Nav/>
-            <div className="pdf-container" style={{ 
-                width: "100%", 
-                maxWidth: "1200px", 
-                margin: "0 auto",
-                padding: "20px"
-            }}>
+            <Navbar/>
+            <div className="pdf-container">
                 <object
                     data={`${MagazinePDF}#view=FitH`}
                     type="application/pdf"
                     width="100%"
                     height="800px"
-                    style={{ 
-                        border: "1px solid lightgray",
-                        minHeight: "800px"
-                    }}
+                    className="pdf-viewer"
                 >
                     <iframe
                         src={`${MagazinePDF}#view=FitH`}
                         width="100%"
                         height="800px"
-                        style={{ border: "1px solid lightgray" }}
+                        className="pdf-fallback"
                         title="Magazine PDF"
                     >
                         <p>
                             Your browser doesn't support PDF viewing. 
-                            <a href={MagazinePDF}>Download the PDF</a> instead.
+                            <a href={MagazinePDF} className="pdf-download-link">Download the PDF</a> instead.
                         </p>
                     </iframe>
                 </object>
