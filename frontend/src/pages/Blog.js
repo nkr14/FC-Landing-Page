e197ce693a0219f67bda2192194470a9aa18bc21
nkr14 <241602045+nkr14@users.noreply.github.com>
2026-03-17 16:20:12 -0400

    Update Blog.js

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -59,7 +59,7 @@ const Blog = () => {
         fontSize: '56px', 
         fontWeight: '500', 
         lineHeight: '1.1',
-        textTransform: 'none'
+        textTransform: 'none',
         margin: '0',
         color: '#ffffff',
       }}>
